bdedde57b58c50be25123dcffe15a0f88be8aba8
WotilaCarneiro <wotila@gmail.com>
2024-07-03 21:02:50 -0300

    Segundo Commit - Desafio concluído

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('Olá mundo!')
+let nomeHeroi = "Mitzvar";
+let pontosXp = 8000;
+let nivelXp = "";
+
+if (pontosXp < 1000) {
+    nivelXp = 'Ferro';
+} else if (pontosXp >= 1000 && pontosXp <= 2000) {
+    nivelXp = 'Bronze';
+} else if (pontosXp >= 2001 && pontosXp <= 5000) {
+    nivelXp = 'Prata';
+} else if (pontosXp >= 5001 && pontosXp <= 6000) {
+    nivelXp = 'Ouro';
+} else if (pontosXp >= 6001 && pontosXp <= 7000) {
+    nivelXp = 'Platina';
+} else if (pontosXp >= 7001 && pontosXp <= 8000) {
+    nivelXp = 'Diamante';
+} else if (pontosXp >= 8001 && pontosXp <= 9000) {
+    nivelXp = 'Ascendente';
+} else if (pontosXp >= 9001 && pontosXp <= 10000) {
+    nivelXp = 'Imortal';
+} else if (pontosXp > 10000) {
+    nivelXp = 'Radiante';
+} else {
+    nivelXp = 'Nível Desconhecido';
+}
+
+console.log(`O herói de nome ${nomeHeroi} está no nível ${nivelXp} com ${pontosXp} pontos`);
+
